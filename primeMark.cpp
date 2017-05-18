806154fc609b41bdfbe16937ad6035f5c66c9507
Drew Ripberger <drew.ripberger@gmail.com>
2017-05-18 09:50:51 -0400

    Update primeMark.cpp

--- primeMark.cpp
+++ primeMark.cpp
@@ -2,11 +2,12 @@
 #include <cstdlib>
 #include <thread>
 #include <time.h>
+#include <fstream>
 
-int main(int argc, char ** argv){
-	double target = double(atoi(argv[1]));
-	double startTime = time(NULL);
-	for(int i = 2; i < target; i++){
+void prime(double limit){
+	std::ofstream results;
+	results.open("results.txt");
+	for(int i = 1; i < limit; i++){
 		bool foundPrime = true;
 		for(int n = 2; n < i; n++){
 			if(i % n == 0){
@@ -15,9 +16,15 @@ int main(int argc, char ** argv){
 			}
 		}
 		if(foundPrime){
-			std::cout << i << std::endl;
+			results << i << std::endl;
 		}
 	}
+}
+
+int main(int argc, char ** argv){
+	double target = double(atoi(argv[1]));
+	double startTime = time(NULL);
+	prime(target);
 	double endTime = time(NULL);
 	double duration = endTime - startTime;
 	std::cout << "done in " << duration << "s" << std::endl;
